5e07c13bb9177f99138ea05ca3fff56649ad8f4c
Hemanth Anil <32560935+mranna@users.noreply.github.com>
2025-07-19 10:59:12 -0700

    Update onPrem_fileupload.ps1

--- src/onPrem_fileupload.ps1
+++ src/onPrem_fileupload.ps1
@@ -1,1 +1,12 @@
+# This script connects to a SQL Server instance, runs a query to select all records from the dbo.test table,
+# and exports the results to a CSV file at E:\Temp\testuploadfile.csv
 
+$server = "<<Insert Server Name>>"
+$database = "<<Insert Database Name>>"
+$query = "<<Insert your query>>" # SELECT * FROM dbo.test
+
+$outputCsv = "<<Insert the folder path and file Name>>" # E:\Temp\testuploadfile.csv
+
+Invoke-Sqlcmd - ServerInstance $server -Database $database -Query $query | Export-Csv -Path $outputCsv -NoTypeInformation -Encoding UTF8
+
+Write-Host "Exported the data to $outputCsv"
